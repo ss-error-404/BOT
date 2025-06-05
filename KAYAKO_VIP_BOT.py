cb8de9334f54da4ac145bdc69d808727a96b3d01
ss-error-404 <rahd.ry+04@zohomail.com>
2025-06-05 23:10:54 +0800

    Update KAYAKO_VIP_BOT.py

--- KAYAKO_VIP_BOT.py
+++ KAYAKO_VIP_BOT.py
@@ -72,7 +72,7 @@ async def search_command(update: Update, context: CallbackContext):
         return await update.message.reply_text("âŒ Kailangan mo ng valid na key, gago!")
 
     if len(context.args) < 1:
-        return await update.message.reply_text("âš  Usage: /search <domain> <lines>
+        return await update.message.reply_text("⚠ Usage: /search <domain> <lines>")
 Example: /search 100082 10")
 
     selected_domain = context.args[0].lower()
